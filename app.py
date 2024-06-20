16c0f09edc95e39ce408c71a534546ada81c760a
pooja-shinde-24 <poojashinde.0112@gmail.com>
2024-06-20 15:14:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title('Hello pooja, nice to meet you.')
+
+st.sidebar.title('my first sample app in streamlit.')
+st.sidebar.subheader('This is made by pooja shinde.')
+
+st.write('Hi pooja, this is your first app. Nice to see you here. Please visit again.')
